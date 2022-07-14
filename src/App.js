104aa07aebdf093f5c620cdfaae18f54af3655ab
Nick Engelhardt <enickgoku2@gmail.com>
2022-07-14 10:00:51 -0400

    particles background

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import ParticleBackground from 'react-particle-backgrounds'
 import Navigation from './components/Navigation'
 import Logo from './components/Logo'
 import ImageLinkForm from './components/ImageLinkForm'
 import Rank from './components/Rank'
 import './App.css'
 
+
+const settings4 = {
+  particle: {
+    particleCount: 100,
+    color: "red",
+    minSize: 2,
+    maxSize: 4
+  },
+  velocity: {
+    directionAngle: 0,
+    directionAngleVariance: 30,
+    minSpeed: 0.2,
+    maxSpeed: 4
+  },
+  opacity: {
+    minOpacity: 0,
+    maxOpacity: 0.5,
+    opacityTransitionTime: 5000
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <ParticleBackground className="particles" settings={settings4} />
       <Navigation />
       <Logo />
       <Rank />
